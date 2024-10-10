c633714f1d2b08f7680669b3f57fa9c39c7a8fff
Patryk <vexim2002@gmail.com>
2024-10-10 18:59:51 +0200

    feat/login

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void Main() {
         System.out.println("Hello World");
         System.out.println("Hello World1");
+        System.out.println("Hello World1");
+        System.out.println("Hello World1");
     }
 
 }
